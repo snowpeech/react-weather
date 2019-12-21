50efd72812557dbf5be2e60c509d1b74ac0acf42
Lydia <snowpeech@gmail.com>
2019-12-21 14:48:14 -0600

    created weather tiles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const API_KEY = "e7a46a487ec841a4bfb1ff4f9f7d72bf";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [query, getQuery] = useState("");
+
+  const updateSearch = event => {
+    setSearch(event.target.value);
+    // console.log(search);
+  };
+
+  const submitSearch = event => {
+    event.preventDefault();
+    getQuery(search);
+
+    setSearch("");
+    //console.log(query);
+  };
+
+  const getData = async () => {
+    console.log("get Data", query);
+    if (query) {
+      const response = await fetch(
+        `https://api.weatherbit.io/v2.0/forecast/daily?postal_code=${query}&country=US&key=${API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello
+      <form>
+        {" "}
+        Enter your zip code:
+        <input type='text' value={search} onChange={updateSearch}></input>
+        <button type='submit' onClick={submitSearch}>
+          search
+        </button>
+      </form>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Weather =()=> {
+return(
+    
+)
+}
+
+export default Weather; 
